3e93c3ba759ffd2534468b866416a86da7be5cac
Paulo Guilherme <paulo.guilherme.arruda1@gmail.com>
2022-08-17 19:47:54 -0300

    fix style

--- src/Utils/usePreventForm.js
+++ src/Utils/usePreventForm.js
@@ -4,7 +4,7 @@ export default function usePreventForm({formRef}){
 
     useEffect(()=> {
         document.body.addEventListener('keypress', function (event) {
-            const key = event.key;
+            const {key} = event;
             if(key === 'Enter'){
                 event.preventDefault();
                 formRef.current?.submitForm();
